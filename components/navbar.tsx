4b03fd1b621ab2e36e05d8dfa7b176d32499296a
Mark Pekala <mpekala@college.harvard.edu>
2023-01-19 10:57:42 -0500

    narrow scope and debug prod spotify

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,6 +23,8 @@ export default function Navbar() {
     const doSpotify = async () => {
       try {
         const refreshToken = process.env.NEXT_PUBLIC_SPOTIFY_REFRESH;
+        console.log("got to refresh token");
+        console.log(refreshToken);
         const res = await fetch("https://accounts.spotify.com/api/token", {
           method: "POST",
           body: `refresh_token=${refreshToken}&grant_type=refresh_token&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_ID}&client_secret=${process.env.NEXT_PUBLIC_SPOTIFY_SECRET}`,
@@ -30,7 +32,12 @@ export default function Navbar() {
             "Content-Type": "application/x-www-form-urlencoded",
           },
         });
+        console.log("res");
+        console.log(res);
         const clean = await res.json();
+        console.log("clean");
+        console.log(clean);
+
         spotifyRef.current = new SpotifyWebApi({
           accessToken: clean.access_token,
         });
@@ -47,7 +54,6 @@ export default function Navbar() {
         let timeString1 = "Right now, ";
         let timeString2 = "I'm";
         let timeString3 = " listening to ";
-        console.log(diffMins, diffHours);
         if (diffMins > 2 && diffHours < 1) {
           timeString1 = `${diffMins} minutes ago `;
           timeString2 = "I";
@@ -68,6 +74,7 @@ export default function Navbar() {
           timeString3,
         });
       } catch (e) {
+        console.log("falling back because", e);
         const fallbackSong = {
           artist: "R.A.P. Ferreira",
           artistUrl: "https://open.spotify.com/artist/2U1vwQRYQmG7ypKJF1JTEb",
@@ -94,57 +101,65 @@ export default function Navbar() {
           className="sm:hidden"
           buttons={[
             { text: "About / Home", onClick: goTo("/") },
-            { text: "Blog", onClick: goTo("blog") },
+            // { text: "Blog", onClick: goTo("blog") },
             { text: "Experience", onClick: goTo("experience") },
-            { text: "Projects", onClick: goTo("projects") },
+            // { text: "Projects", onClick: goTo("projects") },
             { text: "Contact", onClick: goTo("contact") },
           ]}
         />
       </div>
       <div className="">
-        <div className="flex mb-4 items-center text-xs">
-          <p className={`${lastSong ? "" : "hidden"}`}>
-            {lastSong?.timeString1 || ""}
-            <SLink
-              className={`${lastSong ? "" : "hidden"}`}
-              target="_blank"
-              href="https://open.spotify.com/user/mpek66?si=8e586285aca84768"
-            >
-              {lastSong?.timeString2 || ""}
-            </SLink>
-            {lastSong?.timeString3 || ""}
-            <SLink
-              className={`${lastSong ? "" : "hidden"}`}
-              target="_blank"
-              href={lastSong?.trackUrl || ""}
-            >
-              {lastSong?.title || ""}
-            </SLink>{" "}
-            by{" "}
-            <SLink
-              className={`${lastSong ? "" : "hidden"}`}
-              target="_blank"
-              href={lastSong?.artistUrl || ""}
-            >
-              {lastSong?.artist || ""}
-            </SLink>
-            .
-          </p>
-        </div>
+        {lastSong ? (
+          <div className="flex mb-4 items-center text-xs">
+            <p className={`${lastSong ? "" : "hidden"}`}>
+              {lastSong?.timeString1 || ""}
+              <SLink
+                className={`${lastSong ? "" : "hidden"}`}
+                target="_blank"
+                href="https://open.spotify.com/user/mpek66?si=8e586285aca84768"
+              >
+                {lastSong?.timeString2 || ""}
+              </SLink>
+              {lastSong?.timeString3 || ""}
+              <SLink
+                className={`${lastSong ? "" : "hidden"}`}
+                target="_blank"
+                href={lastSong?.trackUrl || ""}
+              >
+                {lastSong?.title || ""}
+              </SLink>{" "}
+              by{" "}
+              <SLink
+                className={`${lastSong ? "" : "hidden"}`}
+                target="_blank"
+                href={lastSong?.artistUrl || ""}
+              >
+                {lastSong?.artist || ""}
+              </SLink>
+              .
+            </p>
+          </div>
+        ) : (
+          <div className="flex w-2/3 h-4 mb-4 animate-pulse bg-slate-300" />
+        )}
       </div>
       <div className="hidden sm:flex flex-row">
         <SLink className="text-lg mr-8" href="/">
           About
         </SLink>
-        <SLink className="text-lg mr-8" href="blog">
-          Blog
-        </SLink>
+        {false && (
+          <SLink className="text-lg mr-8" href="blog">
+            Blog
+          </SLink>
+        )}
         <SLink className="text-lg mr-8" href="experience">
           Experience
         </SLink>
-        <SLink className="text-lg mr-8" href="projects">
-          Projects
-        </SLink>
+        {false && (
+          <SLink className="text-lg mr-8" href="projects">
+            Projects
+          </SLink>
+        )}
         <SLink className="text-lg mr-8" href="contact">
           Contact
         </SLink>
