871a320978874cbcc17769574e1920eb13bb8206
Mark Pekala <mpekala@college.harvard.edu>
2023-01-25 09:52:27 -0500

    mobile friendly projects

--- components/proj_item.tsx
+++ components/proj_item.tsx
@@ -17,9 +17,16 @@ export default function ProjItem({
 
   return (
     <div className="w-full mb-8 p-8 border bg-[rgba(0,0,0,0.3)] border-white">
-      <div className="flex justify-center items-center">
+      <div className="flex flex-wrap sm:flex-nowrap justify-center items-center">
         <div className="mr-8">
           <p className="text-xl font-bold">{title}</p>
+          <Image
+            className="block my-[1.25em] sm:hidden"
+            src={`/proj_imgs/${image}`}
+            alt={title}
+            width={200}
+            height={200}
+          />
           <p className="italic">{timeframe}</p>
           <p className="mb-[1.25em]">{description}</p>
           <div className="flex">
@@ -37,6 +44,7 @@ export default function ProjItem({
           </div>
         </div>
         <Image
+          className="hidden sm:block"
           src={`/proj_imgs/${image}`}
           alt={title}
           width={200}
