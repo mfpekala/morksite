8af2a664f25c6e59269d2e1861a51c52299146c4
Mark Pekala <mpekala@college.harvard.edu>
2023-07-12 21:27:58 -0700

    save state

--- games/welcome.tsx
+++ games/welcome.tsx
@@ -1,8 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
-interface BaseShape {
+interface Vec2 {
   x: number;
   y: number;
+}
+
+interface BaseShape {
+  pos: Vec2;
+  vel: Vec2;
   color: string;
   type: "rect" | "circle";
 }
@@ -20,45 +25,98 @@ interface Rect extends BaseShape {
 
 type Shape = Circle | Rect;
 
-function GameCanvas({ shapes }: { shapes: Shape[] }) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+interface Player {
+  shape: Shape;
+  canJump: boolean;
+}
+
+interface Input {
+  up: boolean;
+  down: boolean;
+  left: boolean;
+  right: boolean;
+}
+
+function InputController() {
+  const inputRef = useRef<Input>({
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+  });
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, []);
-
-  const drawFrame = useCallback(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    shapes.forEach((shape) => {
-      ctx.fillStyle = shape.color;
-      switch (shape.type) {
-        case "rect":
-          ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
+    const keyDownListener = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+        case "w":
+          inputRef.current.up = true;
           break;
-        case "circle":
-          ctx.beginPath();
-          ctx.arc(
-            shape.x + shape.radius / 2,
-            shape.y + shape.radius / 2,
-            shape.radius / 2,
-            0,
-            2 * Math.PI
-          );
-          ctx.fill();
+        case "ArrowDown":
+        case "s":
+          inputRef.current.down = true;
+          break;
+        case "ArrowLeft":
+        case "a":
+          inputRef.current.left = true;
+          break;
+        case "ArrowRight":
+        case "d":
+          inputRef.current.right = true;
           break;
       }
-    });
-  }, [shapes]);
+    };
+    const keyUpListener = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+        case "w":
+          inputRef.current.up = false;
+          break;
+        case "ArrowDown":
+        case "s":
+          inputRef.current.down = false;
+          break;
+        case "ArrowLeft":
+        case "a":
+          inputRef.current.left = false;
+          break;
+        case "ArrowRight":
+        case "d":
+          inputRef.current.right = false;
+          break;
+      }
+    };
+    window.addEventListener("keydown", keyDownListener);
+    window.addEventListener("keyup", keyUpListener);
+    return () => {
+      window.removeEventListener("keydown", keyDownListener);
+      window.removeEventListener("keyup", keyUpListener);
+    };
+  }, []);
+
+  return inputRef;
+}
+
+export default function WelcomeGame() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const playerRef = useRef<Player>({
+    shape: {
+      pos: {
+        x: 0,
+        y: 0,
+      },
+      vel: {
+        x: 0,
+        y: 0,
+      },
+      radius: 10,
+      color: "blue",
+      type: "circle",
+    },
+    canJump: false,
+  });
+
+  const inputRef = InputController();
 
   // Listen for resize events
   useEffect(() => {
@@ -66,6 +124,8 @@ function GameCanvas({ shapes }: { shapes: Shape[] }) {
     if (!canvas) return;
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
+    canvas.width = canvas.clientWidth / 10;
+    canvas.height = canvas.clientHeight / 10;
     const resizeListener = () => {
       canvas.width = canvas.clientWidth;
       canvas.height = canvas.clientHeight;
@@ -75,25 +135,61 @@ function GameCanvas({ shapes }: { shapes: Shape[] }) {
     return () => window.removeEventListener("resize", resizeListener);
   }, []);
 
-  useEffect(() => {
+  const handleInput = useCallback(() => {
+    const player = playerRef.current;
+    const input = inputRef.current;
+    if (input.up && player.canJump) {
+      player.shape.vel.y = -10;
+      player.canJump = false;
+    }
+    if (input.left) {
+      player.shape.vel.x = -5;
+    }
+    if (input.right) {
+      player.shape.vel.x = 5;
+    }
+  }, []);
+
+  const doUpdate = useCallback(() => {
+    handleInput();
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const player = playerRef.current;
+    player.shape.pos.x += player.shape.vel.x;
+    player.shape.pos.y += player.shape.vel.y;
+    // Gravity
+    player.shape.vel.y += 0.5;
+    // Floor
+    if (player.shape.pos.y > canvas.height) {
+      player.shape.pos.y = canvas.height;
+      player.shape.vel.y = 0;
+      player.canJump = true;
+    }
+  }, [handleInput]);
+
+  const drawFrame = useCallback(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
-    // Clear the screen
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.imageSmoothingEnabled = false;
+
+    const shapes = [];
+    shapes.push(playerRef.current.shape);
+
     shapes.forEach((shape) => {
       ctx.fillStyle = shape.color;
       switch (shape.type) {
         case "rect":
-          ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
+          ctx.fillRect(shape.pos.x, shape.pos.y, shape.width, shape.height);
           break;
         case "circle":
           ctx.beginPath();
           ctx.arc(
-            shape.x + shape.radius / 2,
-            shape.y + shape.radius / 2,
+            shape.pos.x + shape.radius / 2,
+            shape.pos.y + shape.radius / 2,
             shape.radius / 2,
             0,
             2 * Math.PI
@@ -102,41 +198,26 @@ function GameCanvas({ shapes }: { shapes: Shape[] }) {
           break;
       }
     });
-  }, [shapes]);
+  }, []);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      className="w-full h-full absolute"
-      width={canvasRef.current?.width}
-      height={canvasRef.current?.height}
-    />
-  );
-}
+  const gameLoop = useCallback(() => {
+    doUpdate();
+    drawFrame();
+  }, [doUpdate, drawFrame]);
 
-export default function WelcomeGame() {
-  const [shapes, setShapes] = useState<Shape[]>([
-    {
-      x: 0,
-      y: 0,
-      width: 10,
-      height: 10,
-      color: "red",
-      type: "rect",
-    },
-    {
-      x: 100,
-      y: 100,
-      width: 100,
-      height: 100,
-      color: "blue",
-      type: "rect",
-    },
-  ]);
+  useEffect(() => {
+    const interval = setInterval(gameLoop, 1000 / 60);
+    return () => clearInterval(interval);
+  }, [gameLoop]);
 
   return (
     <div className="w-full h-full z-50 bg-white absolute">
-      <GameCanvas shapes={shapes} />
+      <canvas
+        ref={canvasRef}
+        className="w-full h-full absolute"
+        width={canvasRef.current?.width}
+        height={canvasRef.current?.height}
+      />
     </div>
   );
 }
