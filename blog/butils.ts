179b6f4b7ea2feb4bc7b779f7d780a0adc9b08e6
Mark Pekala <mpekala@college.harvard.edu>
2023-07-27 00:19:30 -0700

    better progress

--- blog/butils.ts
+++ blog/butils.ts
@@ -1,7 +1,45 @@
+import { Database } from "sqlite3";
 import { BlogPost } from "../types/types";
 
 export async function getBlogPostBySlug(
   slug: string
 ): Promise<BlogPost | null> {
-  return null;
+  const db = new Database("blog/meta.db");
+  const row = await new Promise<any>((resolve, reject) => {
+    db.get("SELECT * FROM posts WHERE slug = ?", [slug], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+  const clean = (row: any): BlogPost | null => {
+    if (!row) {
+      return null;
+    }
+    return {
+      slug: row.slug,
+      title: row.title,
+      abstract: row.abstract,
+      cartoon: row.cartoon,
+      date: row.date,
+      content: JSON.parse(row.content),
+    };
+  };
+  return row;
+}
+
+export async function getAllSlugs(): Promise<{ slug: string }[]> {
+  const db = new Database("blog/meta.db");
+  const rows = await new Promise<{ slug: string }[]>((resolve, reject) => {
+    db.all("SELECT slug FROM posts", (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows as { slug: string }[]);
+      }
+    });
+  });
+  return rows;
 }
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,4 +1,6 @@
 import "react";
+import { getBlogPostBySlug, getAllSlugs } from "../../blog/butils";
+import { BlogPost } from "../../types/types";
 
 interface StaticParams {
   params: {
@@ -6,4 +8,23 @@ interface StaticParams {
   };
 }
 
-export async function getStaticProps({ params }: StaticParams) {}
+export async function getStaticProps({ params }: StaticParams) {
+  const post = await getBlogPostBySlug(params.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const slugs = await getAllSlugs();
+  return {
+    paths: slugs.map((slug) => ({ params: slug })),
+    fallback: false,
+  };
+}
+
+export default function Post({ post }: { post: BlogPost | null }) {
+  return <div>Post</div>;
+}
